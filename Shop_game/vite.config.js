2ddadd47243f916cef6f4e98b46bfd313b21e169
Ketan Raman Ghungralekar <ketanramanghungralekar@Ketans-MacBook-Pro.local>
2026-04-15 00:56:49 +0530

    Configure GitHub Pages deployment for multiple apps

--- Shop_game/vite.config.js
+++ Shop_game/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Global_South_Games_for_intellectually_disabled/Shop_game/',
   plugins: [react()],
 })
