a16d39d85760ff4f6eb91e4e79f593cb731ecb26
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-04-15 10:15:00 +0530

    try smth-4

--- Shop_game/vite.config.js
+++ Shop_game/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Global_South_Games_for_intellectually_disabled/Shop_game/',
